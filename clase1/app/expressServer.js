fad63218a8c081c4157c2ac53f37b2b812c5e9de
Hector Flores <hecto932@gmail.com>
2014-12-25 00:43:16 -0430

    Usando el motor de plantillas swig

--- clase1/app/expressServer.js
+++ clase1/app/expressServer.js
@@ -1,4 +1,5 @@
 var express = require('express'),
+	swig = require('swig'),
 	middlewares = require('./middlewares/admin');
 
 var ExpressServer = function(config){
@@ -11,8 +12,16 @@ var ExpressServer = function(config){
         this.expressServer.use(middlewares[middleware]);
     }
 
+    //DE ESTA MANERA LE DECIMOS A EXPRESS QUE USE SWIG COMO MOTOR DE HTML
+    this.expressServer.engine('html', swig.renderFile);
+    //COMO TEMPLATE DE VISTA EL ENGINE USE HTML
+    this.expressServer.set('view engine', 'html');
+    //DONDE VAN A ENCONTRAR LOS TEMPLATES
+    this.expressServer.set('views', __dirname + '/website/views/templates');
+
 	this.expressServer.get('/article/save/', function(req, res, next){
-		res.send('Hello from article save');
+		res.render('article_save', {nombre: 'diego'});
+		//res.send('Hello from article save');
 	});
 
 	this.expressServer.get('/article/list/', function(req, res, next){
